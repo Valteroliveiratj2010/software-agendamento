07fc03960f8a049d1ba2fbde08e66683c283e970
SeuNomeDeUsuario <valteroliveiratj32@gmail.com>
2025-06-06 12:08:53 -0300

    arquivos atualizados

--- backend/routes/clientesRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clienteController = require('../controllers/clienteController');
-const autenticarToken = require('../middleware/auth');
-
-router.post('/', autenticarToken, clienteController.cadastrarCliente);
-router.get('/', autenticarToken, clienteController.listarClientes);
-
-module.exports = router;
